03bd57a3c4602e72e7d685d3ebf79b3f7d75278d
ihsingh2 <ihsingh2@gmail.com>
2023-03-04 13:34:59 +0530

    final commit

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -9,12 +9,14 @@
 // }
 
 if (localStorage.getItem('mode') == null) localStorage.setItem('mode','light'); // assume light mode for new users
-else if (localStorage.getItem('mode') == 'dark') toggleLightDarkMode(); // apply "preferred" dark mode over the default light mode
+if (localStorage.getItem('mode') == 'dark' & document.body.classList.contains("light-mode")) toggleLightDarkMode(); // apply "preferred" dark mode over the default light mode
 
 function toggleLightDarkMode() {
 	// get the body element
 	const body = document.body;
 	const isLightMode = body.classList.contains("light-mode");
+	if (isLightMode) localStorage.setItem('mode','dark');
+	else localStorage.setItem('mode','light')
 	const iconClass = isLightMode ? "bx bx-sun dark-btn" : "bx bx-moon dark-btn";
 	const elementClass = isLightMode ? "dark-modalName" : "light-modalName";
 	document.getElementById("icon1").className = iconClass;
